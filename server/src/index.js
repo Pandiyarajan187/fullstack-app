262736ea3e3abeabd9b13a9bfe5746708476d1de
smartproject345 <149381810+smartproject345@users.noreply.github.com>
2026-06-20 21:59:16 +0530

    add simple crud apis

--- server/src/index.js
+++ server/src/index.js
@@ -1,5 +1,27 @@
 const express = require('express');
 const app = express();
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
+const User = require('./models/User');
+
+
+dotenv.config();
+
+const mongoURI = process.env.MONGO_URI;
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(mongoURI);
+        console.log('MongoDB connected');
+    } catch (err) {
+        console.error(err.message);
+        process.exit(1);
+    }
+}
+connectDB();
+app.use(express.json());
+
 
 app.get('/api/health', (req, res) => {
     res.send({
@@ -8,6 +30,42 @@ app.get('/api/health', (req, res) => {
     });
 });
 
-app.listen(5000, () => {
-    console.log('Server is running on port 5000');
+app.post('/api/users', async (req, res) => {
+    const { name, email, password, role } = req.body;
+
+    try {
+        const newUser = new User({ name, email, password, role });
+        await newUser.save();
+        res.status(201).json(newUser);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+app.get('/api/users', async (req, res) => {
+    try {
+        const users = await User.find();
+        res.json(users);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+app.delete('/api/users/:id', async (req, res) => {
+    try {
+        const user = await User.findByIdAndDelete(req.params.id);
+        if (!user) {
+            return res.status(404).json({ msg: 'User not found' });
+        }
+        res.json({ msg: 'User deleted', user });
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
 });
